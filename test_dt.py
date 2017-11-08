617b75b74964a0e9bf2b4b3c57f92deaba31cbe2
Chris Hodapp <hodapp87@gmail.com>
2017-11-08 13:59:47 -0500

    Update Decision Tree gridsearch for more params

--- test_dt.py
+++ test_dt.py
@@ -40,7 +40,9 @@ dt = sklearn.tree.DecisionTreeClassifier()
 params = {
     'max_depth': (2,3,4,5,6,7,8,9,10,12,14,16,18,20,23,26,30,35,40,None),
     'min_samples_split': (2, 4, 8, 16),
-    'min_impurity_decrease': (0.0, 0.05, 0.1),
+    'min_samples_leaf': (2, 4, 8, 16),
+    'min_impurity_decrease': (0.0, 0.1, 0.2, 0.3, 0.4),
+    'max_feature': (None, 'auto', 'log2'),
 }
 clf = sklearn.model_selection.GridSearchCV(dt, params, verbose=100)
 clf.fit(train_X, train_y)
